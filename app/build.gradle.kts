0ab77d244616360a7d46439b1e6c7e37b68852f4
Quan Zhu <zhuquan7237@qq.com>
2026-09-17 14:15:46 +0800

    NOFX-QUEST-4JYNQGQXAB 发布 v0.3.2 人物消息强提醒

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.messageatlas.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 6
-        versionName = "0.3.1"
+        versionCode = 7
+        versionName = "0.3.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
