c5d7bcd77831a4d8dd8304bc0bd215fe6602ccbd
Quan Zhu <zhuquan7237@qq.com>
2026-09-19 00:17:38 +0800

    feat: refresh app icon for v0.4.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.messageatlas.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 9
-        versionName = "0.4.0"
+        versionCode = 10
+        versionName = "0.4.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
